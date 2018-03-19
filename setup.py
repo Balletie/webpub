be1bbbee24747ff7e8dda169d3784fd24dd0f813
Balletie <skippetie@gmail.com>
2018-03-19 18:18:35 +0100

    Move main script to cli.py in webpub package

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     entry_points={
-        'console_scripts': ['webpub = webpub:main'],
+        'console_scripts': ['webpub = webpub.cli:main'],
     },
     install_requires=[
         'lxml',
